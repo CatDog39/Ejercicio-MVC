3c49d407b66ff26d581c9b4a0ba27991c56e4ffd
Santiago Alberto Botello Catimay <sabotello@personalsoft.com.co>
2023-02-23 09:04:18 -0500

    ejercicio de mvc

---
+++ src/main/java/com/mvcspringreactor/service/FiguraService.java
@@ -0,0 +1,28 @@
+package com.mvcspringreactor.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mvcspringreactor.modelo.FiguraEntity;
+import com.mvcspringreactor.repository.FakeRepository;
+
+@Service
+public class FiguraService {
+	@Autowired
+	public FakeRepository repositorio;
+	
+	public boolean crearFigura(FiguraEntity figura) {
+		return repositorio.agregarFigura(figura);
+	}
+	
+	public List<FiguraEntity> obtenerFiguras(){
+		return repositorio.traerFiguras();
+	}
+	
+	public boolean eliminarFigura(int id) {
+		return repositorio.eliminarFigura(id);
+	}
+	
+}
---
+++ src/main/java/com/mvcspringreactor/service/SaludoService.java
@@ -0,0 +1,27 @@
+package com.mvcspringreactor.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SaludoService {
+
+	public String hola() {
+		try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "Hola sincrono";
+	}
+	
+	public String hola2() {
+		try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "Hola sincrono 2";
+	}
+}
+
+
